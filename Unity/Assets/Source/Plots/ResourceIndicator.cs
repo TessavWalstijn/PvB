986983e9eb2a3c87713143cf37b6abf5ae0b6ed1
Papventje <robbierobsnor@gmail.com>
2019-05-07 10:03:45 +0200

    added resource tower prototype

---
+++ Unity/Assets/Source/Plots/ResourceIndicator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceIndicator : MonoBehaviour
+{
+    [SerializeField] private Image loadingBar;
+
+    void Start()
+    {
+        loadingBar.fillAmount = 0;
+    }
+
+    void Update()
+    {
+        loadingBar.fillAmount += 0.005f;
+    }
+}
--- Unity/Assets/Source/Towers/Demo_LookAtEnemies.cs
+++ Unity/Assets/Source/Towers/Demo_LookAtEnemies.cs
@@ -5,12 +5,6 @@ public class Demo_LookAtEnemies : MonoBehaviour
     [SerializeField] private GameObject _turretHead;
 
     private GameObject _enemy;
-    [SerializeField] private Vector3 _startRotation;
-
-    void Start()
-    {
-        _startRotation = new Vector3(_turretHead.transform.rotation.eulerAngles.x, _turretHead.transform.rotation.eulerAngles.y, _turretHead.transform.rotation.eulerAngles.z);
-    }
 
     void OnTriggerStay(Collider other)
     {
@@ -24,6 +18,5 @@ public class Demo_LookAtEnemies : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         _enemy = null;
-        _turretHead.transform.localRotation = Quaternion.Euler(_startRotation.x, _startRotation.y, _startRotation.z);
     }
 }
