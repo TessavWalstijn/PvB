85fb9d476bee602dbf6a7044e0eceec606669edb
RobinDeGroot <robindegroot.rdg@gmail.com>
2019-05-08 12:13:46 +0200

    updated rapid fire tower

--- Unity/Assets/Source/Towers/_bullet.cs
+++ Unity/Assets/Source/Towers/_bullet.cs
@@ -2,9 +2,8 @@
 
 public class _bullet : MonoBehaviour
 {
-
-    private Transform _target;
-    protected private float _speed = 70f;
+    private GameObject _target;
+    protected private float _speed = 0.3f;
      void Start()
     {
       _baseTower _enemy = new _baseTower(); 
@@ -12,6 +11,7 @@ public class _bullet : MonoBehaviour
     public void Chase(GameObject _enemy)
     {
         _target = _baseTower._enemy;
+        Debug.Log("Chasing");
     }
 
     
@@ -23,18 +23,19 @@ public class _bullet : MonoBehaviour
             return;
         }
 
-        Vector3 dir = _target.position - transform.position;
+        Vector3 dir = _target.gameObject.transform.position - transform.position;
         float distanceThisFrame = _speed * Time.deltaTime;
 
         if (dir.magnitude <= distanceThisFrame)
         {
             HitTarget();
-            return;
+            //return;
         }
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
     }
     private void HitTarget()
     {
         Debug.Log("Target hit");
+        Destroy(gameObject);
     }
 }
