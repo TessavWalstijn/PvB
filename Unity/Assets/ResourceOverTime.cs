3aee57818c4a3180c22341f1a16c8bc6327530bd
RobinDeGroot <robindegroot.rdg@gmail.com>
2019-05-10 15:03:11 +0200

    added resources over time

---
+++ Unity/Assets/ResourceOverTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceScript : MonoBehaviour
+{
+    private int _resources;
+    private float _startTime;
+    [SerializeField]private Text _resourceText;
+    [SerializeField]private int _rsIncreasepersecond;
+    
+    void Start()
+    {
+        _resources = 0;
+        _startTime = Time.time;
+    }
+
+    void Update()
+    {
+        _resources = _rsIncreasepersecond * (int)(Time.time - _startTime);
+        _resourceText.text = "resources:" + _resources;
+    }
+}
